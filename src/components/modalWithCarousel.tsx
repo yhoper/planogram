5506c3e54ac24a0265b2087dd31269a8b4e8a9bc
Mperez <>
2024-04-16 12:54:18 -0400

    Add first blocks. Change class component by funtional component install modal and carousel

---
+++ src/components/modalWithCarousel.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+interface ModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+  images: string[];
+}
+
+const ModalWithCarousel: React.FC<ModalProps> = ({
+  isOpen,
+  onRequestClose,
+  images,
+}) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const settings = {
+    centerMode: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    afterChange: (index: number) => setCurrentSlide(index),
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Modal"
+      id="centerModal"
+    >
+      <div className="contentCloseModal">
+      <button className="closeModal" onClick={onRequestClose}>X</button>
+      </div>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
+      <p>
+        Foto: {currentSlide + 1} de {images.length}
+      </p>
+    </Modal>
+  );
+};
+
+export default ModalWithCarousel;
--- src/components/planogramad.tsx
+++ src/components/planogramad.tsx
@@ -1,4 +1,5 @@
-import { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
+import ModalWithCarousel from "./modalWithCarousel";
 import _ from "lodash";
 import RGL, { WidthProvider } from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
@@ -8,6 +9,9 @@ import lg from "../assets/lg.svg";
 import marley from "../assets/marley.webp";
 import sony from "../assets/sony.png";
 import samsung from "../assets/samsung.svg";
+import img_1 from "../assets/locales/img_1.jpg";
+import img_2 from "../assets/locales/img_2.jpg";
+import img_3 from "../assets/locales/img_3.jpg";
 
 const ReactGridLayout = WidthProvider(RGL);
 
@@ -26,173 +30,202 @@ interface Props {
   cols?: number;
   rowHeight?: number;
   onLayoutChange?: (layout: LayoutItem[]) => void;
-  verticalCompact?: boolean;
+  compactType?: "horizontal" | "vertical" | null;
   preventCollision?: boolean;
 }
 
-interface State {
-  layout: LayoutItem[];
-}
+const NoCollisionLayout: React.FC<Props> = ({
+  className = "layout",
+  cols = 48,
+  rowHeight = 30,
+  onLayoutChange = () => {},
+  compactType = "vertical",
+  preventCollision = true,
+}) => {
+  const [layout, setLayout] = useState<LayoutItem[]>([]);
 
-class NoCollisionLayout extends PureComponent<Props, State> {
-  static defaultProps: Partial<Props> = {
-    className: "layout",
-    items: 0,
-    cols: 24, // Cambiado a 24 columnas para ajustar el tamaño
-    rowHeight: 30,
-    onLayoutChange: () => {},
-    verticalCompact: false,
-    preventCollision: true,
+  useEffect(() => {
+    setLayout(generateLayout());
+  }, []);
+
+  const generateLayout = (): LayoutItem[] => [
+    {
+      x: 0,
+      y: 0,
+      w: 4,
+      h: 3,
+      i: "R-1",
+      bgColor: "#a50034",
+    },
+    {
+      x: 4,
+      y: 0,
+      w: 8,
+      h: 2,
+      i: "R-2",
+      bgColor: "#a50034",
+    },
+    {
+      x: 12,
+      y: 0,
+      w: 6,
+      h: 4,
+      i: "R-3",
+      bgColor: "#a50034",
+    },
+    {
+      x: 18,
+      y: 0,
+      w: 10,
+      h: 1,
+      i: "SAMSUNG",
+      bgColor: "#a50034",
+    },
+
+    {
+      x: 38,
+      y: 0,
+      w: 10,
+      h: 1,
+      i: "PHILIPS",
+      bgColor: "#a50034",
+    },
+
+    {
+      x: 28,
+      y: 0,
+      w: 10,
+      h: 1,
+      i: "LG",
+      bgColor: "#a50034",
+    },
+
+    {
+      x: 19,
+      y: 1,
+      w: 4,
+      h: 1,
+      i: "R-4.A",
+      bgColor: "#a50034",
+    },
+    {
+      x: 19,
+      y: 2,
+      w: 4,
+      h: 1,
+      i: "R-4.C",
+      bgColor: "#a50034",
+    },
+
+    {
+      x: 24,
+      y: 1,
+      w: 4,
+      h: 2,
+      i: "R-4.B",
+      bgColor: "#a50034",
+    },
+  ];
+
+  const itemModal = (i: any) => {
+    console.log(i);
+    setIsOpen(true);
   };
 
-  constructor(props: Props) {
-    super(props);
+  const generateDOM = () =>
+    _.map(layout, (item) => (
+      <div
+        onClick={() => itemModal(item.i)}
+        key={item.i}
+        style={{ backgroundColor: item.bgColor }}
+      >
+        <span className="text">{item.i}</span>
+      </div>
+    ));
 
-    this.state = { layout: this.generateLayout() };
-  }
-
-  generateDOM() {
-    return _.map(this.state.layout, (item) => {
-      return (
-        <div key={item.i} style={{ backgroundColor: item.bgColor }}>
-          <span className="text">{item.i}</span>
-        </div>
-      );
-    });
-  }
-
-  generateLayout(): LayoutItem[] {
-    return [
-      {
-        x: 0, // Primer bloque en la columna 1
-        y: 0,
-        w: 5,
-        h: 3,
-        i: "75QNED99SPA",
-        bgColor: "#a50034",
-      },
-      {
-        x: 3, // Segundo bloque en la columna 3
-        y: 0,
-        w: 4,
-        h: 2,
-        i: "SOUND. SH7Q",
-        bgColor: "#a50034",
-      },
-      {
-        x: 2, // Tercer bloque en la columna 6
-        y: 0,
-        w: 3,
-        h: 1,
-        i: "65QNED90SPA",
-        bgColor: "#a50034",
-      },
-      {
-        x: 12, // Cuarto bloque en la columna 12
-        y: 3, // Más abajo
-        w: 6,
-        h: 2,
-        i: "75QNED90SPA",
-        bgColor: "#a50034",
-      },
-      {
-        x: 0, // Quinto bloque en la columna 0 (al principio)
-        y: 3, // Más abajo
-        w: 3,
-        h: 8,
-        i: "65OLED G2",
-        bgColor: "#a50034",
-      },
-
-      {
-        x: 8, // Quinto bloque en la columna 0 (al principio)
-        y: 7, // Más abajo
-        w: 6,
-        h: 2,
-        i: "PARL.RP4",
-        bgColor: "#a50034",
-      },
-    ];
-  }
-
-  onLayoutChange(layout: RGL.Layout[]): void {
+  const onLayoutChangeHandler = (layout: RGL.Layout[]): void => {
     const newLayout = layout.map((item) => ({
       x: item.x,
       y: item.y,
       w: item.w,
       h: item.h,
       i: item.i,
-      bgColor: 'item.bgColor',
+      bgColor: "",
     }));
-    this.props.onLayoutChange!(newLayout);
-  }
+    onLayoutChange(newLayout);
+  };
 
-  // Función para agregar un nuevo item con el color especificado
-  addNewItem(color: string): void {
+  const addNewItem = (color: string): void => {
     const newItem: LayoutItem = {
       x: 0,
       y: 0,
       w: 2,
       h: Math.ceil(Math.random() * 4) + 1,
-      i: `item-${this.state.layout.length}`,
+      i: `item-${layout.length}`,
       bgColor: color,
     };
-    this.setState((prevState) => ({
-      layout: [...prevState.layout, newItem],
-    }));
-  }
+    setLayout([...layout, newItem]);
+  };
+  const [isOpen, setIsOpen] = useState(false);
+  const images = [img_1, img_2, img_3];
 
-  render() {
-    return (
-      <>
-        <div className="button-container">
-          <button
-            className="btnglobal color-w"
-            id="lg"
-            onClick={() => this.addNewItem("#a50034")}
-          >
-            <img src={lg} alt="" />
-          </button>
-          <button
-            className="btnglobal color-w"
-            id="sony"
-            onClick={() => this.addNewItem("#000")}
-          >
-            <img src={sony} alt="" />
-          </button>
-          <button
-            className="btnglobal color-w"
-            id="samsung"
-            onClick={() => this.addNewItem("#14299f")}
-          >
-            <img src={samsung} alt="" />
-          </button>
-          <button
-            className="btnglobal color-b"
-            id="bose"
-            onClick={() => this.addNewItem("#f7f8f8")}
-          >
-            <img src={bose} alt="" />
-          </button>
-          <button
-            className="btnglobal color-w"
-            id="marley"
-            onClick={() => this.addNewItem("#1c1c28")}
-          >
-            <img src={marley} alt="" />
-          </button>
-        </div>
-        <ReactGridLayout
-          cols={this.props.cols}
-          layout={this.state.layout}
-          onLayoutChange={(layout) => this.onLayoutChange(layout)}
-          {...this.props}
+  return (
+    <>
+      <ModalWithCarousel
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        images={images}
+      />
+      <div className="button-container">
+        <button
+          className="btnglobal color-w"
+          id="lg"
+          onClick={() => addNewItem("#a50034")}
         >
-          {this.generateDOM()}
-        </ReactGridLayout>
-      </>
-    );
-  }
-}
+          <img src={lg} alt="" />
+        </button>
+        <button
+          className="btnglobal color-w"
+          id="sony"
+          onClick={() => addNewItem("#000")}
+        >
+          <img src={sony} alt="" />
+        </button>
+        <button
+          className="btnglobal color-w"
+          id="samsung"
+          onClick={() => addNewItem("#14299f")}
+        >
+          <img src={samsung} alt="" />
+        </button>
+        <button
+          className="btnglobal color-b"
+          id="bose"
+          onClick={() => addNewItem("#f7f8f8")}
+        >
+          <img src={bose} alt="" />
+        </button>
+        <button
+          className="btnglobal color-w"
+          id="marley"
+          onClick={() => addNewItem("#1c1c28")}
+        >
+          <img src={marley} alt="" />
+        </button>
+      </div>
+      <ReactGridLayout
+        cols={cols}
+        layout={layout}
+        onLayoutChange={(layout) => onLayoutChangeHandler(layout)}
+        className={className}
+        rowHeight={rowHeight}
+        compactType={compactType}
+        preventCollision={preventCollision}
+      >
+        {generateDOM()}
+      </ReactGridLayout>
+    </>
+  );
+};
 
 export default NoCollisionLayout;
